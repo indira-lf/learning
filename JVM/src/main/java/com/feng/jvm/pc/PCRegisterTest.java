36058a8a23f4443aaf83a629db64650852d99e20
liao-lingfeng <f15079447849@163.com>
2022-07-15 23:11:24 +0800

    程序计数器

---
+++ JVM/src/main/java/com/feng/jvm/pc/PCRegisterTest.java
@@ -0,0 +1,18 @@
+package com.feng.jvm.pc;
+
+/**
+ * @author
+ * @time 2022/7/15 21:48
+ * @Description- TODO
+ */
+public class PCRegisterTest {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+
+        int k = i + j;
+
+        String a = "abc";
+        System.out.println(a);
+    }
+}
