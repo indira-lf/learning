8632af406486048d6c1ba521d1b50a46021c4a0a
liao-lingfeng <f15079447849@163.com>
2022-09-27 22:54:22 +0800

    两个字符串相加

---
+++ algorithm/src/main/java/com/feng/algorthm/other/AddStringDemo.java
@@ -0,0 +1,33 @@
+package com.feng.algorthm.other;
+
+/**
+ * @author
+ * @time 2022/9/27 22:45
+ * @Description- 字符串相加
+ */
+public class AddStringDemo {
+
+    public static void main(String[] args) {
+
+        String s = addStrings("12", "2");
+        System.out.println(s);
+    }
+
+    public static String addStrings(String num1, String num2) {
+        int i = num1.length() - 1, j = num2.length() - 1, add = 0;
+        StringBuffer ans = new StringBuffer();
+        while (i >= 0 || j >= 0 || add != 0) {
+            int x = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int y = j >= 0 ? num2.charAt(j) - '0' : 0;
+            int result = x + y + add;
+            ans.append(result % 10);
+            add = result / 10;
+            i--;
+            j--;
+        }
+        // 计算完以后的答案需要翻转过来
+        ans.reverse();
+        return ans.toString();
+    }
+
+}
